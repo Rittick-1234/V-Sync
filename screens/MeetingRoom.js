7a907b75d4094d762f222971f2eea63f9df205f3
Rittick Agarwal <rittick.agarwal@nagarro.com>
2022-06-10 11:07:35 +0530

    first commit

--- screens/MeetingRoom.js
+++ screens/MeetingRoom.js
@@ -1,36 +1,113 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, TextInput, Text } from "react-native";
+import { View, StyleSheet, TextInput, Text ,Alert, SafeAreaView, TouchableOpacity} from "react-native";
 import StartMeeting from "../components/StartMeeting";
 import { io } from "socket.io-client";
+import { Camera} from "expo-camera" ;
+import FontAwesome from "react-native-vector-icons/FontAwesome"
 
+const menuIcons = [
+  {
+  id : 1 ,
+  name : "microphone" ,
+  title : " Mute " ,
+  customColor : " #efefef " ,
+  } ,
+  {
+    id : 2 ,
+    name : "video-camera" ,
+    title : " Stop Video " ,
+  },
+  {
+      id : 3 ,
+      name : "upload" ,
+      title : "Share Content" ,
+  } ,
+  {
+      id : 4 ,
+      name : "group" ,
+      title : " Participants " ,  
+  }
+] 
+
+let socket;
 function MeetingRoom() {
   const [name, setName] = useState();
   const [roomId, setRoomId] = useState();
-  const [activeUsers, setActiveUsers] = useState();
+  const [activeUsers, setActiveUsers] = useState ([ ] );
+  const [ startCamera , setStartCamera ] = useState ( false ) ;
+
+  const __startCamera = async ( ) => {
+      const { status } = await Camera.requestCameraPermissionsAsync() ;
+      if ( status === "granted" ) {
+        setStartCamera (true) ;
+      }else{
+          Alert.alert ( " Access denied " ) ;
+      }
+    }
 
   const joinRoom = () => {
-    socket.emit("join-room", { roomId: roomId, userName: name });
+    __startCamera();
+    socket.emit("join-room", { roomId:roomId, userName:name });
   };
 
   useEffect(() => {
     //const API_URL = "https://356b-103-220-210-164.in.ngrok.io";
-    socket = io("https://81f4-103-220-210-164.in.ngrok.io");
+    socket = io("  https://0a19-103-87-141-28.in.ngrok.io ");
     socket.on("connection", () => console.log("connected"));
     socket.on("all-users", users => {
-      console.log("Active Users");
-      console.log(users);
+      console.log (users," After clean up " )
       setActiveUsers(users);
     });
   }, []);
   return (
     <View style={styles.container}>
-      <StartMeeting
-        name={name}
-        setName={setName}
-        roomId={roomId}
-        setRoomId={setRoomId}
-        joinRoom={joinRoom}
-      />
+          {/* Start meeting Section */}
+      { startCamera ? (
+          <SafeAreaView style={{flex: 1}}>
+            <View style={styles.activeUsersContainer}>
+                  <View style={styles.cameraContainer}>
+                      <Camera
+                          type = { "front" }
+                          style = 
+                          { { 
+                                width : activeUsers.length<=1? "100%":200 , 
+                                height : activeUsers.length<=1?600:200 
+                          } }
+                      >
+                      </Camera>
+                      { activeUsers.filter (user=>(user.userName!=name)).map ( ( user , index ) =>
+                        < View key = {index} style = { styles.activeUserContainer } >
+                            < Text style = { { color : "white" } } > {user.userName } </Text>
+                        </View>
+                      ) }
+                  </View>
+              </View>
+              {/* Footer */}
+              < View style = { styles.menu } >
+                    {menuIcons.map((icon,index)=>
+                        <TouchableOpacity
+                            key={index}
+                            style={styles.tile}
+                        >
+                              <FontAwesome name = {icon.name} size = { 24 } color = {"#efefef"}/>
+                              < Text style = {styles.textTile} >{icon.title}</Text>
+                        </TouchableOpacity>
+                    )}
+              </View >
+          </SafeAreaView>
+          
+      ) : (
+       <StartMeeting
+              name={name}
+              setName={setName}
+              roomId={roomId}
+              setRoomId={setRoomId}
+              joinRoom={joinRoom}
+     />
+      )
+    }
+      
+     
     </View>
   );
 }
@@ -41,5 +118,42 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#1c1c1c",
     flex: 1
-  }
+  },
+  tile : {
+    justifyContent : "center" ,
+    alignItems : "center" ,
+    height : 50 ,
+    marginTop : 15
+  },
+  textTile:{
+    color:"white",
+    marginTop:10
+  },
+  menu:{
+    flexDirection:"row",
+    justifyContent:"space-around"
+  },
+  cameraContainer:{
+    // flex:1,
+    // backgroundColor:"black",
+    justifyContent:"center",
+    flexDirection:"row",
+    flexWrap:"wrap",
+    alignItems:"center"
+  },
+  activeUsersContainer:{
+      flex:1,
+      width:"100%",
+      justifyContent:"center",
+      alignItems:"center",
+      backgroundColor:"black"
+  },
+  activeUserContainer : {
+    borderColor : "gray" ,
+    borderWidth : 1 ,
+    width : 200 ,
+    height : 200 ,
+    justifyContent:"center",
+    alignItems:"center"
+  } 
 });
